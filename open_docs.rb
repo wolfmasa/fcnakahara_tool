00ab0cf4cb66c465efb8943d1b2df6c40116803e
Masa Jobara <wolf.masa@gmail.com>
2016-04-18 00:57:05 +0900

    学年のスケジュール取得まで完了

--- open_docs.rb
+++ open_docs.rb
@@ -1,23 +1,67 @@
 require 'open-uri'
 require 'json'
 
-google_document_url = 'https://spreadsheets.google.com/feeds/cells/1aSE2HDWMOtGMKdvuplF8zaFIJYWWGgMJ-_qtZJG322Y/od6/public/values?alt=json'
-key = '1aSE2HDWMOtGMKdvuplF8zaFIJYWWGgMJ-_qtZJG322Y'
+url = 'https://spreadsheets.google.com/feeds/cells/1aSE2HDWMOtGMKdvuplF8zaFIJYWWGgMJ-_qtZJG322Y/od6/public/values?alt=json'
 
-f = open(google_document_url)
-buff = JSON.parse(f.read)
-f.close
-
-entry = buff["feed"]["entry"]
-
-list = []
-entry.each do |e|
-  cell = e["gs$cell"]
-  row = cell["row"]
-  col = cell["col"]
-  text = cell["$t"]
-
-  list << "#{row}, #{col} => #{text}"
+def open_doc url
+  f = open(url)
+  buff = JSON.parse(f.read)
+  f.close
+  buff
 end
 
-p list
+def parse_doc_json buff
+  entry = buff["feed"]["entry"]
+
+  list = []
+  entry.each do |e|
+    cell = e["gs$cell"]
+    row = cell["row"].to_i
+    col = cell["col"].to_i
+    text = cell["$t"]
+
+    list[row] = [] if list[row].nil?
+    list[row] << {row: row, col: col, text: text}
+
+  end
+  list
+end
+
+def get_schedule_list list
+  list.select! do |l|
+    (! l.nil?) and
+      (!l.first.nil?) and
+      (/^\d{2}\(/ === l.first[:text])
+  end
+
+  schedule = []
+  list.each do |l|
+    result = l.find { |item| item[:col] == TARGET_COL}
+    if result
+      schedule << "#{l.first[:text]}: #{result[:text]}"
+
+    else
+      (TARGET_COL-1).downto(3) do |i|
+        result = l.find { |item| item[:col] == i}
+        if result
+          schedule << "#{l.first[:text]}: #{result[:text]}"
+          break
+        end
+      end
+    end
+  end
+  schedule
+end
+
+
+
+buff = open_doc url
+contents_list = parse_doc_json buff
+
+
+TARGET_COL = 5
+schedule = get_schedule_list contents_list
+
+require 'pp'
+pp schedule
+
