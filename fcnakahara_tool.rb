8d08ecee9553eb76ceda52f7287fa49c92e0195f
jobara <joubara.masashi@canon.co.jp>
2016-04-28 12:58:03 +0900

    rename ruby file with class name

---
+++ fcnakahara_tool.rb
@@ -0,0 +1,6 @@
+require File.expand_path('open_docs.rb', File.dirname(__FILE__))
+require File.expand_path('calendar.rb', File.dirname(__FILE__))
+require File.expand_path('parse_html.rb', File.dirname(__FILE__))
+
+
+
